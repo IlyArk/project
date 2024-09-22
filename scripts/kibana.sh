04079610e02da64a91e5ad54f1ba91f11fe6de84
IlyArk <ilyasov.arkadii@inbox.ru>
2024-09-22 20:01:00 +0300

    Delete scripts directory

--- scripts/kibana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /etc/project/kibana
-
-cp kibana.yml /etc/kibana/
-
-sudo systemctl restart kibana.service
--- scripts/prometheus-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /etc/project/prometheus
-
-sudo cp prometheus.yml /etc/prometheus/
-
-sudo systemctl restart prometheus
-sudo systemctl restart grafana-server
